b46213016f4a8ea3999f6108b7495e17939ee441
razdobudko.s <razdobudko.s@wb.ru>
2023-05-07 22:55:13 +0200

    Task 1 - Rotation

--- app/src/main/java/com/mcmouse88/customdiagram/MainActivity.kt
+++ app/src/main/java/com/mcmouse88/customdiagram/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.mcmouse88.customdiagram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.mcmouse88.customdiagram.ui.DiagramView
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.tv_info)
         val diagram = findViewById<DiagramView>(R.id.diagram)
         val button = findViewById<Button>(R.id.animateButton)
         diagram.data = listOf(
@@ -24,27 +19,8 @@ class MainActivity : AppCompatActivity() {
             500f
         )
 
-        val onAnimateClick = View.OnClickListener {
-            diagram.startAnimation(
-                AnimationUtils.loadAnimation(this, R.anim.animation).apply {
-                    setAnimationListener(object : Animation.AnimationListener {
-
-                        override fun onAnimationStart(animation: Animation?) {
-                            textView.text = "On Animation Start"
-                        }
-
-                        override fun onAnimationEnd(animation: Animation?) {
-                            textView.text = "On Animation End"
-                        }
-
-                        override fun onAnimationRepeat(animation: Animation?) {
-                            textView.text = "On Animation Repeat"
-                        }
-                    })
-                }
-            )
+        button.setOnClickListener {
+            diagram.update()
         }
-
-        button.setOnClickListener(onAnimateClick)
     }
 }
