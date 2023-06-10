6e93b28af2ac6220a120f9ec1ab9d46f2594d6d8
razdobudko.s <razdobudko.s@wb.ru>
2023-06-10 23:46:22 +0200

    Create transition animation

--- app/src/main/java/com/mcmouse88/customdiagram/MainActivity.kt
+++ app/src/main/java/com/mcmouse88/customdiagram/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.mcmouse88.customdiagram
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.mcmouse88.customdiagram.ui.DiagramView
+import androidx.transition.Scene
+import androidx.transition.TransitionManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val diagram = findViewById<DiagramView>(R.id.diagram)
         val button = findViewById<Button>(R.id.animateButton)
-        diagram.data = listOf(
-            500f,
-            500f,
-            500f,
-            500f
-        )
+        val root = findViewById<ViewGroup>(R.id.root)
+
+        val scene = Scene.getSceneForLayout(root, R.layout.end_scene, this)
 
         button.setOnClickListener {
-            diagram.update()
+            TransitionManager.go(scene)
         }
     }
 }
